15a14c153d5a6bcb3f353dac18027ea88650b94b
Christian Jodice <mrjodice@gmail.com>
2025-06-21 02:04:13 -0400

    Lab complete

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,24 @@
 // Code PasswordInput Component Here
+import { useState } from 'react'
 
 function PasswordInput (){
+    const [password, setPassword] = useState('')
+    
+    const handleChange = (e) => {
+        setPassword(e.target.value)
+        console.log("Entering password...")
+    }
+
+    const handleFocus = () => {
+        console.log("Focusing on password input")
+    }
+
+    const handleBlur = () => {
+        console.log("Blurring on password input")
+    }
+    
     return(
-        <></>
+        <input type="password" placeholder="Enter your password" value={password} onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur}/>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,29 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    function handleSubmit(event){
+        event.preventDefault()
+        alert("Password submitted!")
+    }
+
+    function handleEnter() {
+        console.log("Mouse Entering")
+    }
+
+    function handleLeave() {
+        console.log("Mouse Exiting")
+    }
+
     return(
-        <></>
+        <div>
+            <button 
+                onClick={handleSubmit}
+                onMouseEnter={handleEnter}
+                onMouseLeave={handleLeave}
+            >
+                Submit Password
+            </button>
+        </div>
     )
 }
 
